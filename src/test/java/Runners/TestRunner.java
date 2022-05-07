be2550d58773a7b4d177b70fd17f10bc8b1f1b77
thanmai98 <thanmaitummalapalli@gmail.com>
2022-05-07 21:46:11 +0530

    Code Updation

---
+++ src/test/java/Runners/TestRunner.java
@@ -0,0 +1,23 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/FeatureFiles",
+        glue = {"StepDefinition"},
+        plugin = {"pretty", "summary",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true,
+        publish = true
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+//    @Override
+//    @DataProvider(parallel = false)
+//    public Object[][] scenarios{
+//        return super.scenarios();
+//    }
+}
